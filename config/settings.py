f3df1be3226cc0c2f87adfd7fb86de197410c946
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-01 22:32:17 +0800

    fix login

--- config/settings.py
+++ config/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SESSION_COOKIE_AGE = 900  # 15 minutes
 SESSION_SAVE_EVERY_REQUEST = True
