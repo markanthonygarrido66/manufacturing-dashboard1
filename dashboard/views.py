f1d2078a9f172ef22566210ffe08e90f4f89f180
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-02 07:19:28 +0800

    Fixed bugss

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from .models import ProductionLine, YieldRecord
 from django.contrib.auth.decorators import login_required
 from .decorators import allowed_roles
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
 from django.db.models import Sum, Avg
+
 
 @login_required
 @allowed_roles(['manager', 'executive'])
@@ -18,30 +21,54 @@ def dashboard(request):
     start = request.GET.get('start')
     end = request.GET.get('end')
 
-    # CLEAN EMPTY INPUT
-    if line == "":
-        line = None
-
     if line:
-        records = records.filter(production_line_id=line)
+        records = records.filter(
+            production_line_id=line
+        )
 
     if start and end:
-        records = records.filter(production_date__range=[start, end])
+        records = records.filter(
+            production_date__range=[start, end]
+        )
 
-    total_yield = records.aggregate(Sum('yield_percentage'))
-    avg_yield = records.aggregate(Avg('yield_percentage'))
+    total_yield = records.aggregate(
+        Sum('yield_percentage')
+    )
+
+    avg_yield = records.aggregate(
+        Avg('yield_percentage')
+    )
+
+    chart_labels = [
+        str(record.production_date)
+        for record in records
+    ]
+
+    chart_data = [
+        float(record.yield_percentage)
+        for record in records
+    ]
 
     role = None
+
     if hasattr(request.user, 'userprofile'):
         role = request.user.userprofile.role
 
-    return render(request, 'dashboard/dashboard.html', {
-        'production_lines': production_lines,
-        'records': records,
-        'total_yield': total_yield,
-        'avg_yield': avg_yield,
-        'role': role
-    })
+    return render(
+        request,
+        'dashboard/dashboard.html',
+        {
+            'production_lines': production_lines,
+            'records': records,
+            'total_yield': total_yield,
+            'avg_yield': avg_yield,
+            'role': role,
+            'chart_labels': chart_labels,
+            'chart_data': chart_data,
+        }
+    )
+
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def push_yield(request):
@@ -54,4 +81,6 @@ def push_yield(request):
         output=output
     )
 
-    return Response({"message": "Yield recorded"})
+    return Response({
+        "message": "Yield recorded successfully"
+    })
