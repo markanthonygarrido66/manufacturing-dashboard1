0c6bec153ea1caa2c3589ae24777e94bfd0f804f
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-02 08:10:10 +0800

    Fixed

--- config/settings.py
+++ config/settings.py
@@ -14,11 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 # =========================
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-change-this-in-production"
-)
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-key-change-me")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
