bd0081b9a02eb5ca79f0618a5013922794da3c1d
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-02 10:37:33 +0800

    fixed

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
         template_name='registration/login.html'
     )),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+   
     path('production/', views.production_view, name='production'),
     path('materials/', views.materials_view, name='materials'),
     path('sensors/', views.sensors_view, name='sensors'),
