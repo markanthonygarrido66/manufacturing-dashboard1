26ff2afe8c89009193ae927837e94124e593f4a7
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-02 21:46:08 +0800

    Live Update

--- production/views.py
+++ production/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import ProductionRecord
+import random
 
 def production_home(request):
     return render(request, 'production/production_home.html')
 
+from django.http import JsonResponse
+from .models import ProductionRecord
+import random
+
 def production_live(request):
+
     records = ProductionRecord.objects.order_by('-production_date')[:10]
 
     data = []
@@ -13,9 +19,7 @@ def production_live(request):
     for r in records:
         data.append({
             "line": r.production_line.name,
-            "output": r.output,
-            "yield": r.yield_percentage,
-            "defects": r.defects,
+            "output": r.yield_percentage + random.randint(-2, 2),
             "date": str(r.production_date)
         })
 
