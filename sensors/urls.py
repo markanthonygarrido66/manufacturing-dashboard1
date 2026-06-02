3fe96c6407d8ad02a50d64af34dcd98d9961328e
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-02 15:46:59 +0800

    Sensors Fixed

--- sensors/urls.py
+++ sensors/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.sensor_dashboard, name='sensor_dashboard'),
     path('api/push/', views.push_sensor_data, name='push_sensor_data'),
+    path('sensors/', views.sensor_dashboard, name='sensor_dashboard')
 ]
