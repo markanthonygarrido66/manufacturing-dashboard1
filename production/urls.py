b4146a1e9c03fffd825f85c2895f804d292966c7
garridomarkanthony58-coder <garridomarkanthony58@gmail.com>
2026-06-03 10:05:28 +0800

    add user

--- production/urls.py
+++ production/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 ),
 path("production/input/", views.production_input_page),
 path("api/production/input/", views.production_input_api),
-
+ path('input/', views.production_input_page, name='production_input_page'),
+    path('api/input/', views.production_input_api, name='production_input_api'),
 ]
